75816172d8fa2bedb4b1f05cc012356036c7d31b
mohammadanas51 <mohammad.anas51@hotmail.com>
2026-05-22 20:00:26 +0530

    Incorporated try catch and conditional chaining

--- api.py
+++ api.py
@@ -3,14 +3,24 @@ import requests
 baseURL = "https://api.restful-api.dev/"
 
 def getObjectsById(id):
-    url = f"{baseURL}/objects/{id}"
-    response = requests.get(url)
-    if(response.status_code == 200):
-        print("API Response", response)
-        jsonResponse = response.json()
-        print("JSON response", jsonResponse)
-    else:
-        print("Something went wrong")
+    try:
+        url = f"{baseURL}/objects/{id}"
+        response = requests.get(url)
+        if(response.status_code == 200):
+            print("API Response", response)
+            jsonResponse = response.json()
+            print("JSON response", jsonResponse)
+            print("Device Name ", jsonResponse["name"])
+            capacityExists = jsonResponse.get("data", {}).get("Capacity")
+            if capacityExists:
+                print("Device Capacity ", jsonResponse["data"]["Capacity"])
+            else:
+                print("Other data ", jsonResponse["data"])
+    except:
+        print("Something went wrong ")
 
+def main():
+    id = input("Enter the device's Id ")
+    getObjectsById(id)
 
-getObjectsById(10)
+main()
